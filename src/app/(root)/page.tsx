37fa20b8dbecdabc3715908873509d98bc19a04b
jakareya76 <jakareya1306@gmail.com>
2025-03-09 11:19:22 +0600

    create the project structure and header and footer

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,9 @@
 import { Button } from "@/components/ui/button";
 
+export const metadata = {
+  title: "Home",
+};
+
 const HomePage = () => {
   return (
     <div className="h2-bold">
---
+++ src/lib/constants/index.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "SnapBuy";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "SnapBuy a modern ecommerce platform build with Next.js";
+
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
