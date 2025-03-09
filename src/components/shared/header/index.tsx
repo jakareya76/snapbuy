4a123b2f78637e2a1b1191f05182e5084e61903d
jakareya76 <jakareya1306@gmail.com>
2025-03-09 12:42:45 +0600

    make the menu mobile responsive

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,9 +1,8 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+
+import Menu from "./menu";
 import { APP_NAME } from "@/lib/constants";
-import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -24,19 +23,7 @@ const Header = () => {
           </Link>
         </div>
 
-        <div className="space-x-2 flex items-center">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/card">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
---
+++ src/components/shared/header/menu.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import ModeToggle from "./mode-toggle";
+import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const Menu = () => {
+  return (
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-1">
+        <ModeToggle />
+        <Button asChild variant="ghost">
+          <Link href="/card">
+            <ShoppingCart /> Cart
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href="/sign-in">
+            <UserIcon /> Sign In
+          </Link>
+        </Button>
+      </nav>
+
+      <nav className="md:hidden">
+        <Sheet>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+
+          <SheetContent className="flex flex-col items-start p-5">
+            <SheetTitle>Menu</SheetTitle>
+            <div className="flex items-center justify-around w-full">
+              <ModeToggle />
+              <Button asChild variant="ghost">
+                <Link href="/card">
+                  <ShoppingCart /> Cart
+                </Link>
+              </Button>
+              <Button asChild>
+                <Link href="/sign-in">
+                  <UserIcon /> Sign In
+                </Link>
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  );
+};
+
+export default Menu;
