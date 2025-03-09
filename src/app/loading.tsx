89892d2c7b7d4a8ec4ecd4fef70922b5356a9495
jakareya76 <jakareya1306@gmail.com>
2025-03-09 12:06:19 +0600

    create the loading and not found

---
+++ src/app/loading.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import loader from "@/assets/loader.gif";
+
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
+      <Image src={loader} width={48} height={48} alt="Loading..." />
+    </div>
+  );
+};
+
+export default Loading;
