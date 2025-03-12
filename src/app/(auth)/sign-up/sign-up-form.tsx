7f0186ad018fdeecdbe443d02f27bc17d8f4c2bc
jakareya76 <jakareya1306@gmail.com>
2025-03-13 01:41:00 +0600

    completed the auth part of the project

---
+++ src/app/(auth)/sign-up/sign-up-form.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { signUpUser } from "@/lib/actions/user.actions";
+import { useSearchParams } from "next/navigation";
+
+const SignUpForm = () => {
+  const [data, action] = useActionState(signUpUser, {
+    success: false,
+    message: "",
+  });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  const SignUpButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? "Creating The Account..." : "Sign Up"}
+      </Button>
+    );
+  };
+
+  return (
+    <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
+      <div className="space-y-6">
+        <div>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            id="name"
+            name="name"
+            type="name"
+            required
+            autoComplete="name"
+          />
+        </div>
+        <div>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+          />
+        </div>
+        <div>
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+          />
+        </div>
+        <div>
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input
+            id="confirmPassword"
+            name="confirmPassword"
+            type="password"
+            required
+            autoComplete="confirmPassword"
+          />
+        </div>
+        <div>
+          <SignUpButton />
+        </div>
+
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+
+        <div className="text-sm text-center text-muted-foreground">
+          Already have an account?{" "}
+          <Link href="/sign-in" target="_self" className="link">
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default SignUpForm;
--- src/auth.ts
+++ src/auth.ts
@@ -56,9 +56,12 @@ export const config = {
   ],
 
   callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async session({ session, user, trigger, token }: any) {
       // set the user id form the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
 
       // if there is an update, set the user name
       if (trigger === "update") {
@@ -66,6 +69,27 @@ export const config = {
       }
 
       return session;
+    },
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role;
+
+        // if user has no name then use email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      return token;
     },
   },
 } satisfies NextAuthConfig;
