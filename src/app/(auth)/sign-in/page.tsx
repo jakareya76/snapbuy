9acb4277035443e784020de8cdcc89cf5bcef650
jakareya76 <jakareya1306@gmail.com>
2025-03-12 23:31:21 +0600

    create the sign in form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -9,6 +9,7 @@ import { APP_NAME } from "@/lib/constants";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import CredentialsSignInForm from "./credentials.sign-in-form";
 
 export const metadata: Metadata = {
   title: "Sign In",
@@ -35,7 +36,9 @@ const SignInPage = () => {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">{/* Form Here */}</CardContent>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   );
