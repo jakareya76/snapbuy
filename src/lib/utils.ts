d4e0c244e4d1e550ddaedfdb7dc23e215f8a9d81
jakareya76 <jakareya1306@gmail.com>
2025-03-19 13:15:06 +0600

    create admin dashboard overview page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -71,6 +71,13 @@ export function formatCurrency(amount: number | string | null) {
   }
 }
 
+// Format Number
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+
+export function formatNumber(num: number) {
+  return NUMBER_FORMATTER.format(num);
+}
+
 // Shorten UUID
 export function formatId(id: string) {
   return `..${id.substring(id.length - 6)}`;
